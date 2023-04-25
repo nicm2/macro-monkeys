a1efde4ffd1993093fcf71850e5ca4f4dbc9ec56
boply <braedencopley@gmail.com>
2023-04-25 09:18:06 -0700

    card checks

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -2,12 +2,37 @@
 	export let title: string;
 	export let text: string;
 	export let link: string;
+  export let level: string;
+  let completed: Boolean;
+
+  async function Fetch () {
+    fetch("https://monkeybackend.rohanj.dev/api/code/getLevelList", {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+        }
+        })
+        .then(response =>
+            response.json().then(data => {
+              if (data[level] == "Complete") completed = true;
+              else completed = false;
+        }).catch(e => { })
+        )
+        .catch(error => {
+        
+        })
+    }
 </script>
 
 <div class="card w-96 bg-green-700 shadow-xl border-green-800 border-2">
     <div class="card-body">
       <h2 class="card-title">{title}</h2>
       <p>{text}</p>
+      {#if completed}
+        <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check absolute right-7 top-5"><polyline points="20 6 9 17 4 12"></polyline></svg>
+      {/if}
       <div class="card-actions justify-end">
         <a href="/frq/{link}" class="btn btn-primary">Go</a>
       </div>
--- src/routes/(default)/frq/+page.svelte
+++ src/routes/(default)/frq/+page.svelte
@@ -4,18 +4,18 @@
 
 <div class="flex flex-wrap w-screen h-full min-h-screen py-24 px-8 gap-10 bg-green-600">
 
-    <Card title="2018 FRQ 3A" text="Run code for this FRQ" link="2018A"/>
+    <Card title="2018 FRQ 3A" text="Run code for this FRQ" link="2018A" level="0"/>
 
-    <Card title="2018 FRQ 3B" text="Run code for this FRQ" link="2018B"/>
+    <Card title="2018 FRQ 3B" text="Run code for this FRQ" link="2018B" level="1"/>
 
-    <Card title="2017 FRQ 4A" text="Run code for this FRQ" link="2017A"/>
+    <Card title="2017 FRQ 4A" text="Run code for this FRQ" link="2017A" level="2"/>
 
-    <Card title="2017 FRQ 4B" text="Run code for this FRQ" link="2017B"/>
+    <Card title="2017 FRQ 4B" text="Run code for this FRQ" link="2017B" level="3"/>
 
-    <Card title="2016 FRQ A" text="Run code for this FRQ" link="2016A"/>
+    <Card title="2016 FRQ A" text="Run code for this FRQ" link="2016A" level="4"/>
 
-    <Card title="2016 FRQ B" text="Run code for this FRQ" link="2016B"/>
+    <Card title="2016 FRQ B" text="Run code for this FRQ" link="2016B" level="5"/>
 
-    <Card title="Extra Credit" text="Run code for this FRQ" link="extra"/>
+    <Card title="Extra Credit" text="Run code for this FRQ" link="extra" level="6"/>
 
 </div>
