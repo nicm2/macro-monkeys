d81dfb736ea0b1c62c3683a008c5b904d7661c4e
boply <braedencopley@gmail.com>
2023-04-25 00:36:11 -0700

    finalizing fixes

--- src/routes/(default)/+page.svelte
+++ src/routes/(default)/+page.svelte
@@ -1,6 +1,6 @@
 <div class="flex px-8 py-24 bg-green-600 h-screen w-screen">
 
-    <div class="flex flex-col bg-green-700 border-green-800 border-2 rounded-lg shadow-xl w-96 md:w-1/2 h-fit self-center p-8">
+    <div class="flex flex-col bg-green-700 border-green-800 border-2 rounded-lg shadow-xl w-96 md:w-1/2 h-fit p-8">
     
         <p>Visit the FRQ page from the Navbar!</p>
 
