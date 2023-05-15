6dbcf106e1918625cde6cd7544f61cf8b34bd936
supermengman <73082021+supermengman@users.noreply.github.com>
2023-05-15 09:43:43 -0700

    make center

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -81,7 +81,7 @@
               </div>
             </label>
             <ul tabindex="0" class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
-              <li>{userName}</li>
+              <li><button>{userName}</button></li>
               <li><button on:click={Logout} >Logout</button></li>
             </ul>
           </div>
