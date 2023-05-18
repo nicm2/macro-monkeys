37bfa869c895759039867000c844e45f2071d3e8
kiannp44 <kianhero44@gmail.com>
2023-05-18 09:12:40 -0700

    sort buttons

--- src/routes/(default)/frq/+page.svelte
+++ src/routes/(default)/frq/+page.svelte
@@ -2,6 +2,14 @@
     import Card from "../../../components/Card.svelte";
 </script>
 
+<div id="myBtnContainer" class="bg-green-700">
+    <button class="btn active" onclick="filterSelection('all')"> Show all</button>
+    <button class="btn" onclick="filterSelection('mncs')"> Methods and Control Structures</button>
+    <button class="btn" onclick="filterSelection('classes')"> Classes</button>
+    <button class="btn" onclick="filterSelection('arrays')"> Array/ArrayList</button>
+    <button class="btn" onclick="filterSelection('2d')"> 2D Array</button>
+  </div>
+
 <div class="flex flex-wrap w-screen h-full min-h-screen py-24 px-8 gap-10 bg-green-600">
 
     <Card title="2018 FRQ 3A" text="Run code for this FRQ" link="2018A" level="0"/>
