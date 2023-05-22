ba90091cde981fffa897b2d52e302c6dbc891248
nicm2 <nmounier@live.com>
2023-05-22 09:02:58 -0700

    syntax highlighting

--- src/routes/(default)/frq/[level]/+page.svelte
+++ src/routes/(default)/frq/[level]/+page.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
     import 'prismjs/components/prism-java';
+    import 'prismjs';
     /** @type {import('./$types').PageData} */
     export let data: any;
-    let code: String;
-    let prevCode: String;
+    let code: string;
+    let prevCode: string;
 
-    async function RunCode () {
+    async function RunCode() {
         fetch("https://monkeybackend.rohanj.dev/api/code/attemptLevel", {
             method: 'POST',
             credentials: 'include',
@@ -50,36 +51,44 @@
     GetCode();
 
     document.addEventListener('DOMContentLoaded', function() {
-        Prism.highlightAllUnder(document.querySelector('codingArea'));
+        Prism.highlightAll();
     });
-        
-
 </script>
 
 <div class="flex flex-col w-screen h-full justify-start py-24 gap-10 bg-green-600">
-
     <div class="flex self-center w-96 md:w-1/2 gap-8 h-fit">
-
         <div class="flex flex-col self-center bg-yellow-300 border-yellow-600 border-2 rounded-lg shadow-xl w-full h-fit p-8 text-2xl text-center font-bold text-yellow-600">
-    
             <!-- Title for quiz -->
             <p>{data.title}</p>
-        
         </div>
-
     </div>
 
     <div class="flex flex-col bg-green-700 border-green-800 border-2 rounded-lg shadow-xl w-96 md:w-1/2 h-fit self-center p-8">
-    
         <!-- Question for quiz -->
         <p style="white-space: pre-line;">{data.question}</p>
-    
     </div>
 
     <div class="flex flex-col bg-green-700 border-green-800 border-2 rounded-lg shadow-xl w-96 md:w-1/2 h-96 self-center p-8 justify-center gap-8">
         <textarea bind:value={code} class="textarea w-full h-full" placeholder="Code..." id="codingArea"></textarea>
-
         <input type="submit" on:click={RunCode} value="Run" class="btn btn-primary" />
     </div>
-
 </div>
+
+<style>
+    .textarea {
+        background-color: #fff;
+        color: #333;
+        border: 1px solid #ccc;
+        padding: 8px;
+        font-size: 14px;
+    }
+
+    .btn {
+        background-color: #007bff;
+        color: #fff;
+        border: none;
+        padding: 8px 16px;
+        font-size: 16px;
+        cursor: pointer;
+    }
+</style>
