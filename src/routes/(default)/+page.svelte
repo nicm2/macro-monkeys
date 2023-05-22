e8510b8789c0289f1242d9abe02db501fe79f15a
nicm2 <89167131+nicm2@users.noreply.github.com>
2023-05-22 03:17:14 -0700

    Update +page.svelte

--- src/routes/(default)/+page.svelte
+++ src/routes/(default)/+page.svelte
@@ -2,7 +2,9 @@
 
     <div class="flex flex-col bg-green-700 border-green-800 border-2 rounded-lg shadow-xl w-96 md:w-1/2 h-fit p-8">
     
-        <p>Visit the FRQ page from the Navbar!</p>
+        <p>This site is a helpful tool to learn and challenge your knowledge of cs topics</p>
+        <p>Test your abilities by writing code segments to complete FRQ problems</p>
+        <p>Visit the FRQ page from the Navbar to start!</p>
 
     </div>
     
