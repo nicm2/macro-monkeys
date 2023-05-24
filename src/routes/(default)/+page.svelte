9b7a9116f4faf2b65696e5af9fbbec7e6f3b1bb8
nicm2 <89167131+nicm2@users.noreply.github.com>
2023-05-24 02:35:49 -0700

    Update +page.svelte

--- src/routes/(default)/+page.svelte
+++ src/routes/(default)/+page.svelte
@@ -5,10 +5,10 @@
 <div class="flex px-8 py-24 bg-green-600 h-screen w-screen">
     <div class="flex flex-col bg-green-700 border-green-800 border-2 rounded-lg shadow-xl w-96 md:w-1/2 h-fit p-8">
         <h1 class="text-white text-3xl font-bold mb-4">Welcome to Macro Monkey!</h1>
-        <p class="text-white mb-6">Master your macroeconomics skills through practice.</p>
-        <p class="text-white mb-6">Macro Monkey is an interactive platform designed to help you excel in macroeconomics. We provide a collection of Free-Response Question (FRQ) prompts that challenge you to apply your knowledge and coding skills to find correct solutions.</p>
-        <p class="text-white mb-6">Our carefully crafted FRQs cover a wide range of macroeconomic topics, allowing you to deepen your understanding and strengthen your problem-solving abilities.</p>
-        <p class="text-white mb-6">To get started, simply visit the FRQ page from the Navbar above and dive into the world of macroeconomics challenges.</p>
+        <p class="text-white mb-6">Master your computer science FRQ skills through practice.</p>
+        <p class="text-white mb-6">Macro Monkey is an interactive platform designed to help you excel in a variety of computer science topics. We provide a collection of Free-Response Question (FRQ) prompts that challenge you to apply your knowledge and coding skills to find correct solutions.</p>
+        <p class="text-white mb-6">Our carefully crafted FRQs cover a wide range of CS topics, allowing you to deepen your understanding and strengthen your problem-solving abilities.</p>
+        <p class="text-white mb-6">To get started, simply visit the FRQ page from the Navbar above and dive into the world of CS challenges.</p>
         <a href="{frqPageUrl}" class="bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded">Go to FRQ Page</a>
     </div>
     
