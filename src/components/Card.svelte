18ddcf7bab663a789a4007d478062cb8bc94eb43
nicm2 <89167131+nicm2@users.noreply.github.com>
2023-05-24 10:42:56 -0700

    Update Card.svelte

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -32,7 +32,11 @@
         })
     }
 </script>
-
+<style>
+	.category {
+		color: "yellow";
+	}
+</style>
 <div class="card w-96 bg-green-700 shadow-xl border-green-800 border-2">
     <div class="card-body">
       <h2 class="card-title">{title}</h2>
@@ -41,7 +45,7 @@
         <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check absolute right-7 top-5"><polyline points="20 6 9 17 4 12"></polyline></svg>
       {/if}
       {#each categories as category}
-	    <p>{category}</p>
+	    <p class = "category">{category}</p>
       {/each}
       <div class="card-actions justify-end">
         <a href="/frq/{link}" class="btn btn-primary">Go</a>
