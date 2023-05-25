4fdaf8ff2940cee9e1b6e38173804d19f3e5910f
nicm2 <nmounier@live.com>
2023-05-25 05:50:13 -0700

    prism highlighting

--- src/routes/(default)/frq/[level]/+page.svelte
+++ src/routes/(default)/frq/[level]/+page.svelte
@@ -3,7 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/prismjs@1.24.1/themes/prism.min.css">
     <script src="https://cdn.jsdelivr.net/npm/prismjs@1.24.1/prism.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/prismjs/components/prism-java.min.js"></script>
 </head>
 <body>
     <script lang="ts">
@@ -95,5 +97,9 @@
             cursor: pointer;
         }
     </style>
+
+    <script>
+        Prism.highlightAll();
+    </script>
 </body>
 </html>
