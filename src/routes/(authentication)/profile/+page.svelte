0c8406607e0763b4066aae3927d9d49be593a755
nicm2 <nmounier@live.com>
2023-05-25 06:37:42 -0700

    profile page

--- src/routes/(authentication)/profile/+page.svelte
+++ src/routes/(authentication)/profile/+page.svelte
@@ -25,16 +25,60 @@
     }
   </script>
   
-  <!-- Profile Content -->
-  <div class="flex flex-col items-center justify-center h-screen bg-green-600">
-    <div class="max-w-md bg-yellow-300 border-yellow-600 border-2 rounded-lg shadow-xl p-8 text-2xl text-center font-bold text-yellow-600">
-      <!-- Profile Title -->
-      <h1>Welcome, {name}!</h1>
-    </div>
+  <style>
+    body {
+      margin: 0;
+      padding: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      min-height: 100vh;
+      background-color: #34D399;
+      font-family: Arial, sans-serif;
+    }
   
-    <div class="max-w-md mt-8 bg-green-700 border-green-800 border-2 rounded-lg shadow-xl p-8">
-      <!-- Profile Information -->
-      <p class="mb-4">
+    .profile-container {
+      max-width: 400px;
+      padding: 2rem;
+      background-color: #FDE047;
+      border: 2px solid #D69E2E;
+      border-radius: 8px;
+      text-align: center;
+    }
+  
+    .profile-title {
+      font-size: 2rem;
+      font-weight: bold;
+      margin-bottom: 1rem;
+      color: #D69E2E;
+    }
+  
+    .profile-info {
+      margin-bottom: 1rem;
+      color: #065F46;
+    }
+  
+    .logout-button {
+      background-color: #2563EB;
+      color: #fff;
+      border: none;
+      padding: 0.75rem 1.5rem;
+      font-size: 1rem;
+      cursor: pointer;
+      border-radius: 4px;
+      transition: background-color 0.3s ease;
+    }
+  
+    .logout-button:hover {
+      background-color: #1d4ed8;
+    }
+  </style>
+  
+  <div class="profile-container">
+    <h1 class="profile-title">Welcome, {name}!</h1>
+  
+    <div class="profile-info">
+      <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consequat accumsan nunc, in ullamcorper velit
         tristique et. Donec in arcu et risus lacinia facilisis. Morbi in diam ullamcorper, vulputate ligula a, cursus
         libero.
@@ -45,96 +89,6 @@
       </p>
     </div>
   
-    <!-- Logout Button -->
-    <button on:click={() => { window.location.href = '/login'; }} class="btn btn-primary mt-4">Logout</button>
+    <button class="logout-button" on:click={() => { window.location.href = '/login'; }}>Logout</button>
   </div>
-  
-  <style>
-    body {
-      margin: 0;
-      padding: 0;
-    }
-  
-    .flex {
-      display: flex;
-    }
-  
-    .flex-col {
-      flex-direction: column;
-    }
-  
-    .items-center {
-      align-items: center;
-    }
-  
-    .justify-center {
-      justify-content: center;
-    }
-  
-    .h-screen {
-      height: 100vh;
-    }
-  
-    .bg-green-600 {
-      background-color: #34D399;
-    }
-  
-    .max-w-md {
-      max-width: 28rem;
-    }
-  
-    .bg-yellow-300 {
-      background-color: #FDE047;
-    }
-  
-    .border-yellow-600 {
-      border-color: #D69E2E;
-    }
-  
-    .text-yellow-600 {
-      color: #D69E2E;
-    }
-  
-    .bg-green-700 {
-      background-color: #065F46;
-    }
-  
-    .border-green-800 {
-      border-color: #047857;
-    }
-  
-    .text-center {
-      text-align: center;
-    }
-  
-    .text-2xl {
-      font-size: 1.5rem;
-      line-height: 2rem;
-    }
-  
-    .p-8 {
-      padding: 2rem;
-    }
-  
-    .mb-4 {
-      margin-bottom: 1rem;
-    }
-  
-    .btn {
-      background-color: #2563EB;
-      color: #fff;
-      border: none;
-      padding: 0.75rem 1.5rem;
-      font-size: 1rem;
-      cursor: pointer;
-    }
-  
-    .btn-primary {
-      background-color: #2563EB;
-    }
-  
-    .mt-4 {
-      margin-top: 1rem;
-    }
-  </style>
   
