8967430a068d97827b0bd0fcfd3a0d02ccf05fba
nicm2 <nmounier@live.com>
2023-05-25 05:26:14 -0700

    tag styling

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -34,7 +34,15 @@
 </script>
 <style>
 	.category {
-		color: #bfb326;
+		color: #e8d125;
+    border-radius: 15px;
+    background: green;
+    padding-right: 20px;
+    display: inline-block;
+    height: auto;
+    width: fit-content;
+    padding-left: 20px;
+    padding: 10px;
 	}
 </style>
 <div class="card w-96 bg-green-700 shadow-xl border-green-800 border-2">
