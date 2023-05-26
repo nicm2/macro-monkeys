46a789091f17dbb47a2c82e0788158f5120ad077
Rohan Juneja <rohanj2006@gmail.com>
2023-05-25 21:46:14 -0700

    use svelete markdown component

--- src/routes/(default)/frq/[level]/+page.svelte
+++ src/routes/(default)/frq/[level]/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     /** @type {import('./$types').PageData} */
+    import SvelteMarkdown from 'svelte-markdown';
     export let data: any;
     let code: string;
     let prevCode: string;
@@ -61,7 +62,8 @@
 
     <div class="flex flex-col bg-green-700 border-green-800 border-2 rounded-lg shadow-xl w-96 md:w-1/2 h-fit self-center p-8">
         <!-- Question for quiz -->
-        <p style="white-space: pre-line;">{data.question}</p>
+        <!-- <p style="white-space: pre-line;">{data.question}</p> -->
+        <SvelteMarkdown {data.question} />
     </div>
 
     <div class="flex flex-col bg-green-700 border-green-800 border-2 rounded-lg shadow-xl w-96 md:w-1/2 h-96 self-center p-8 justify-center gap-8">
