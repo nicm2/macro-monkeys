170eef5a9a52f6453c331828c1829cb3bc653ad7
Rohan Juneja <rohanj2006@gmail.com>
2023-05-26 09:03:46 -0700

    wrap things

--- src/components/Code.svelte
+++ src/components/Code.svelte
@@ -9,6 +9,12 @@ pre {
     margin-top: 8px;
     margin-bottom: 10px;
     padding: 10px;
+    
+    white-space: pre-wrap;       /* Since CSS 2.1 */
+    white-space: -moz-pre-wrap;  /* Mozilla, since 1999 */
+    white-space: -pre-wrap;      /* Opera 4-6 */
+    white-space: -o-pre-wrap;    /* Opera 7 */
+    word-wrap: break-word;       /* Internet Explorer 5.5+ */
 }
 </style>
 
