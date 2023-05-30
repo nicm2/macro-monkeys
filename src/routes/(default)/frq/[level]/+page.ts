a7b9d9075d21016872bf0383343f4f0c7bd18bdb
Rohan Juneja <rohanj2006@gmail.com>
2023-05-30 08:52:02 -0700

    fix an md

--- src/routes/(default)/frq/[level]/+page.ts
+++ src/routes/(default)/frq/[level]/+page.ts
@@ -44,7 +44,7 @@ The following code segment initializes an array and calls the **getColumn** meth
 
 \`\`\`
 int[][] arr2D = { { 0, 1, 2 } , { 3, 4, 5 } , { 6, 7, 8 } , { 9, 5, 3 } }; 
-int[] result = ArrayTester.getColumn (arr2D, 1)
+int[] result = ArrayTester.getColumn(arr2D, 1);
 \`\`\`
 
 When the code segment has completed execution, the variable result will have the following contents: **{1, 4, 7, 5}**
@@ -52,7 +52,7 @@ When the code segment has completed execution, the variable result will have the
 \`\`\`
 public static int[] getColumn (int[] arr2D, int c) {
 
-    // YOUR CODE IS HERE
+    // YOUR CODE IS INSERTED HERE
 
 }
 \`\`\`
@@ -61,81 +61,88 @@ public static int[] getColumn (int[] arr2D, int c) {
 			break;
 		case "2018B":
 			title = "2018 CSA FRQ 3B";
-			question = `This question involves reasoning about arrays of integers. You will write two static methods, both of which are in a class named ArrayTester.
+			question = `
+This question involves reasoning about arrays of integers. You will write two static methods, both of which are in a class named **ArrayTester**.
+\`\`\`
+public class ArrayTester { 
+	/* 
+	Returns an array containing the elements of column c of arr2D in the same order as they appear in arr2D. 
+	Precondition: c is a valid column index in arr2D. 
+	Postcondition: arr2D is unchanged. 
+	*/ 
+	public static int [] getColumn (int [1 [] arr2D, int c) 
+	{ /* implementation not shown */ } 
 
-			public class ArrayTester { 
-			/* 
-			Returns an array containing the elements of column c of arr2D in the same order as they appear in arr2D. 
-			Precondition: c is a valid column index in arr2D. 
-			Postcondition: arr2D is unchanged. 
-			*/ 
-			public static int [] getColumn (int [1 [] arr2D, int c) 
-			{ /* implementation not shown */ } 
-			
-			/*
-			Returns true if and only if every value in arr1 appears in arr2. 
-			Precondition: arr1 and arr2 have the same length. • Postcondition: arr1 and arr2 are unchanged.
-				*/ 
-			public static boolean hasAllValues (int [] arr1, int [] arr2) 
-			{ /* implementation not shown */ }
-			
-			// Returns true if arr contains any duplicate values; false otherwise. 
-			public static boolean containsDuplicates (int [] arr) 
-			{ /* implementation not shown */ }
-			
-			/*
-			Returns true if square is a Latin square as described in part (b); false otherwise. Precondition: square has an equal number of rows and columns. square has at least one row. 
-			*/ 
-			public static boolean isLatin (int [1 [] square) 
-			{ /* Your code goes here */ } 
-			} 
-			
-			Write the static method isLatin, which returns true if a given two-dimensional square array is a Latin square, and otherwise, returns false. A two-dimensional square array of integers is a Latin square if the following conditions are true. 
-			- The first row has no duplicate values.
-			- All values in the first row of the square appear in each row of the square. 
-			- All values in the first row of the square appear in each column of the square. 
-			Feel free to use functions defined in the ArrayTester class to help you
-			
-			
-			public static boolean isLatin (int[] square) {
-			
-			CODE TEXTBOX
-			
-			}				
+	/*
+	Returns true if and only if every value in arr1 appears in arr2. 
+	Precondition: arr1 and arr2 have the same length. • Postcondition: arr1 and arr2 are unchanged.
+		*/ 
+	public static boolean hasAllValues (int [] arr1, int [] arr2) 
+	{ /* implementation not shown */ }
+
+	// Returns true if arr contains any duplicate values; false otherwise. 
+	public static boolean containsDuplicates (int [] arr) 
+	{ /* implementation not shown */ }
+
+	/*
+	Returns true if square is a Latin square as described in part (b); false otherwise. Precondition: square has an equal number of rows and columns. square has at least one row. 
+	*/ 
+	public static boolean isLatin (int [1 [] square) 
+	{ /* Your code goes here */ } 
+} 
+\`\`\`
+
+Write the static method **isLatin**, which returns true if a given two-dimensional square array is a Latin square, and otherwise, returns false. A two-dimensional square array of integers is a Latin square if the following conditions are true. 
+- The first row has no duplicate values.
+- All values in the first row of the square appear in each row of the square. 
+- All values in the first row of the square appear in each column of the square. 
+Feel free to use functions defined in the ArrayTester class to help you
+
+\`\`\`
+public static boolean isLatin (int[] square) {
+
+	// YOUR CODE IS INSERTED HERE
+
+}
+\`\`\`
 			`;
 			levelNumber = 1;
 			break;
 		case "2017A":
 			title = "2017 CSA FRQ 4A";
-			question = `This question involves reasoning about a two-dimensional (2D) array of integers. You will write two static methods, both of which are in a single enclosing class named Successors (not shown). These methods process a 2D integer array that contains consecutive values. Each of these integers may be in any position in the 2D integer array. For example, the following 2D integer array with 3 rows and 4 columns contains the integers 5 through 16, inclusive. 
+			question = `
+This question involves reasoning about a two-dimensional (2D) array of integers. You will write two static methods, both of which are in a single enclosing class named Successors (not shown). These methods process a 2D integer array that contains consecutive values. Each of these integers may be in any position in the 2D integer array. For example, the following 2D integer array with 3 rows and 4 columns contains the integers 5 through 16, inclusive. 
 
-			|  0   1  2   3
-		  ------------------
-		  0 | 15  5  9  10
-		  1 | 12 16 11 6
-		  2 | 14  8  13 7
-		  
-		  The following Position class is used to represent positions in the integer array. The notation (r, c) will be used to refer to a Position object with row r and column c. 
-		  
-		  public class Position { 
-		   
-		  /** Constructs a Position object with row r and column c. */ 
-		  public Position(int r, int c) 
-		  { / * implementation not shown * / }
-		  
-		  } 
-		  
-		  Write a static method findPosition that takes an integer value and a 2D integer array and returns the position of the integer in the given 2D integer array. If the integer is not an element of the 2D integer array, the method returns null.
-		  For example, assume that array arr is the 2D integer array shown at the beginning of the question. 
-		  - The call findPosition(8, arr) would return the Position object (2,1) because the value 8 appears in art at row 2 and column 1.
-		  - The call findPosition(17, arr) would return null because the value 17 does not appear in arr. 
-		  
-		  
-		  public static Position findPosition(int num, int[][] intArr) {
-		  
-		  USER CODE GOES HERE
-		  
-		  }					  
+| | 0 | 1 | 2 | 3 |
+|-|---|---|---|---|
+|0|15|5|9|10|
+|1|12|16|11|6|
+|2|14|8|13|7|
+
+The following **Position** class is used to represent positions in the integer array. The notation (r, c) will be used to refer to a **Position** object with row r and column c. 
+
+\`\`\`
+public class Position { 
+
+	/** Constructs a Position object with row r and column c. */ 
+	public Position(int r, int c) 
+	{ / * implementation not shown * / }
+
+} 
+\`\`\`
+
+Write a static method **findPosition** that takes an integer value and a 2D integer array and returns the position of the integer in the given 2D integer array. If the integer is not an element of the 2D integer array, the method returns null.
+For example, assume that array arr is the 2D integer array shown at the beginning of the question. 
+- The call findPosition(8, arr) would return the Position object (2,1) because the value 8 appears in art at row 2 and column 1.
+- The call findPosition(17, arr) would return null because the value 17 does not appear in arr. 
+
+\`\`\`
+public static Position findPosition(int num, int[][] intArr) {
+
+	// YOUR CODE IS INSERTED HERE
+
+}
+\`\`\`
 			`;
 			levelNumber = 2;
 			break;
