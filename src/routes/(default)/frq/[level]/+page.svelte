6a0f164d496ec15667f7d1e3a55a14d6ffab11eb
nicm2 <nmounier@live.com>
2023-06-04 02:08:47 -0700

    prism styling

--- src/routes/(default)/frq/[level]/+page.svelte
+++ src/routes/(default)/frq/[level]/+page.svelte
@@ -14,20 +14,20 @@
             method: 'POST',
             credentials: 'include',
             headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
             },
-            body: JSON.stringify({levelNumber: data.levelNumber, code: code})
-            })
+            body: JSON.stringify({ levelNumber: data.levelNumber, code: code })
+        })
             .then(response =>
                 response.json().then(data => {
-                if (data.err) alert(data.err);
-                else if (data.msg) alert(data.msg);
-                else alert("Error Occurred!");
-            }).catch(e => { alert("Error Occurred!")})
+                    if (data.err) alert(data.err);
+                    else if (data.msg) alert(data.msg);
+                    else alert("Error Occurred!");
+                }).catch(e => { alert("Error Occurred!") })
             )
             .catch(error => {
-            alert("Error Occurred!");
+                alert("Error Occurred!");
             })
 
         // once sent, delete code from local storage
@@ -46,20 +46,20 @@
             method: 'POST',
             credentials: 'include',
             headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
             },
-            body: JSON.stringify({level: data.levelNumber})
-            })
+            body: JSON.stringify({ level: data.levelNumber })
+        })
             .then(response =>
                 response.json().then(data => {
-                if (data.err) console.log(data.err);
-                else { (<HTMLTextAreaElement>document.getElementById("codingArea")).value = data.snippet; console.log(data.snippet); code = data.snippet; }
-            }).catch(e => { alert("Error Occurred!")})
+                    if (data.err) console.log(data.err);
+                    else { (<HTMLTextAreaElement>document.getElementById("codingArea")).value = data.snippet; console.log(data.snippet); code = data.snippet; }
+                }).catch(e => { alert("Error Occurred!") })
             )
             .catch(error => {
-            alert("Error Occurred!");
-        })
+                alert("Error Occurred!");
+            })
     }
 
     onMount(GetCode);
@@ -67,8 +67,6 @@
     async function SaveCodeToLocalStorage() {
         localStorage.setItem(codeLocalStorageName, code);
     }
-
-
 </script>
 
 <div class="flex flex-col w-screen h-full justify-start py-24 gap-10 bg-green-600">
@@ -82,14 +80,14 @@
     <div class="flex flex-col bg-green-700 border-green-800 border-2 rounded-lg shadow-xl w-96 md:w-1/2 h-fit self-center p-8">
         <!-- Question for quiz -->
         {#if data.isMarkdown}
-        <SvelteMarkdown source={data.question} renderers={{code: Code}} />
-        {:else} 
+        <SvelteMarkdown source={data.question} renderers={{ code: Code }} />
+        {:else}
         <p style="white-space: pre-line;">{data.question}</p>
         {/if}
     </div>
 
     <div class="flex flex-col bg-green-700 border-green-800 border-2 rounded-lg shadow-xl w-96 md:w-1/2 h-96 self-center p-8 justify-center gap-8">
-        <textarea bind:value={code} on:keyup={SaveCodeToLocalStorage} class="textarea w-full h-full" placeholder="Code..." id="codingArea"></textarea>
+        <textarea bind:value={code} on:keyup={SaveCodeToLocalStorage} class="textarea w-full h-full language-java" placeholder="Code..." id="codingArea"></textarea>
         <input type="submit" on:click={RunCode} value="Run" class="btn btn-primary" />
     </div>
 </div>
@@ -112,3 +110,7 @@
         cursor: pointer;
     }
 </style>
+
+<!-- PrismJS Script and Styles -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.25.0/prism.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.25.0/themes/prism.min.css" />
